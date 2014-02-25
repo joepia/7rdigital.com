e97166404c44712228119b7a40c6418cf8c5005c
Brad Sawicki <git@bradsawicki.com>
2014-02-25 10:00:08 -0600

    Removed Views

--- views/bottom.php
+++
@@ -1,5 +0,0 @@
-	<section id="bottom" class="bottom cf">
-		<div class="wrapper cf">
-			<p>Looking to get started on your web project? <a href="" class="btn contact">Contact me</a></p>
-		</div>	
-	</section>
--- views/head-2.php
+++
@@ -1,5 +0,0 @@
-	<link href="css/fonts/icons.css" rel="stylesheet" media="all">
-	<link href="css/styles.css" rel="stylesheet" media="all">
-	<link rel="shortcut icon" href="images/favicon.png" />
-	<link rel="stylesheet" type="text/css" href="//cloud.typography.com/7766032/711062/css/fonts.css" />
-</head>
